f3e6fa63d28962dd2de93b9b42d106a5db1c10c8
task58 <taskr3g@gmail.com>
2025-05-27 16:22:16 +0900

    " をエスケープ

--- index.js
+++ index.js
@@ -161,6 +161,7 @@ function generateCSVText(chatArr,excludeCharacters = []){
 
 		name.replaceAll(","," ");
 		chatText.replaceAll(","," ");
+        chatText.replaceAll('"'," ");
 
 		outputText += name;
 		outputText += ",";
